ad480e7686d588666360e2a8d322246cbef985dd
Jim Stewart <jhsis@shaw.ca>
2014-02-13 17:17:58 -0800

    initial

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jhsis/newmath"
+)
 
+// hello is an example of a trivial command
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world  Sqrt(2) = %v\n", newmath.Sqrt(2))
+	fmt.Printf("hello again\n")
+	fmt.Printf("good-bye\n")
 }
-
-
